63f7e5bcac7b96c13cc984c473855c37fd601b63
Alex M Abrosimov (SnakeU2) <snakeu2@gmail.com>
2017-12-29 11:31:00 +0300

    first commit

--- calc-oil.php
+++ calc-oil.php
@@ -1,6 +1,20 @@
 <?php
 /*
  * Plugin Name: Calc-Oil
- * Description:  Basic WordPress Plugin Header Comment
+ * Plugin URI: https://github.com/SnakeU2/calc-oil.git
+ * Description:  Calculator for A. Povergo
  * Version:      0.1.0
- * Author:       Alexey M. Abrosimov (snakeu2@gmail.com)
+ * Author:       Alexey M. Abrosimov (snakeu2@gmail.com) 
+ * 
+ */
+
+
+
+ /*
+  * TODO: activate hook - creating tables if not exists
+  * TODO: remove tables
+  * TODO: tables structure
+  * TODO: output
+  * TODO: shortcode
+  * TODO: widget
+  * /
