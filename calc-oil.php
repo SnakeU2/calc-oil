e6b056ba8f44823327599798d03d9e670c715d12
Alex M Abrosimov (SnakeU2) <snakeu2@gmail.com>
2018-01-06 17:31:45 +0300

    getting oil acids & modal form

--- calc-oil.php
+++ calc-oil.php
@@ -8,38 +8,51 @@
  * 
  */
 
-/*----------------------Activation------------------*/
-    register_activation_hook( __FILE__, 'co_install');
-
-    function co_install(){
-        global $wpdb;
-        //Check installed 
-        if (!is_admin() || (int)get_option('Calc_Oil_Installed' === 1)) return false;
-        //Creating tables & insert data
-        if(is_admin()){
-            if(!is_file(__DIR__ . '/calc-oil.sql')){                
-                trigger_error('Отсутствует файл начальных данных calc-oil.sql<br>Пожалуйста, переустановите плагин', E_USER_ERROR);                
-            }       
-            $queries = file(__DIR__ . '/calc-oil.sql');
-            foreach($queries as $query){
-                $query = str_replace("wp_co_",$wpdb->prefix."co_");
-                $wpdb->query($query);
-            }
-            add_option('Calc_Oil_Installed',1);           
-        }        
-        
+/*----------------------Activation & Uninstall-----------------*/
+register_activation_hook( __FILE__, function() use ($wpdb){
+    //Check installed
+    if (!is_admin() || (int)get_option('Calc_Oil_Installed' === 1)) return false;
+    //Creating tables & insert data
+    if(is_admin()){
+        if(!is_file(__DIR__ . '/calc-oil.sql')){
+            trigger_error('Отсутствует файл начальных данных calc-oil.sql<br>Пожалуйста, переустановите плагин', E_USER_ERROR);
+        }
+        $queries = file(__DIR__ . '/calc-oil.sql');
+        foreach($queries as $query){
+            $query = str_replace("wp_co_",$wpdb->prefix."co_");
+            $wpdb->query($query);
+        }
+        add_option('Calc_Oil_Installed',1);
     }
+});
 
 /*--------------------Admin section----------------------*/
 
-add_action('admin_enqueue_scripts', 'calc_oil_adm_scripts');
+add_action('admin_enqueue_scripts', function (){
+    //css
+    wp_register_style('bootstrap',plugins_url('/bootstrap/css/bootstrap.css',__FILE__));
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style( 'co-admin-style', plugins_url('/admin_style.css',__FILE__));
+    //js register   
+	wp_deregister_script( 'jquery' );
+	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');	
+    wp_register_script( 'popper',plugins_url('/bootstrap/js/popper.min.js',__FILE__),array('jquery'));
+    wp_register_script( 'bootstrap', plugins_url('/bootstrap/js/bootstrap.min.js',__FILE__),array('jquery'));
+    wp_register_script( 'co_admin-script', plugins_url('/js/co_admin.js',__FILE__),array('jquery'));
+    //js enqueue
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'popper' );
+	wp_enqueue_script( 'bootstrap' );
+	wp_enqueue_script( 'co_admin-script' );
+    
+});
 
 add_action('admin_menu', function (){
-        add_menu_page( 'Таблица масел', 'Calc-Oil', 'manage_options', 'calc-oils', 'get_oils',"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAQOFAAEDhQGlVDz+AAAAB3RJTUUH4gEDChQt3muYgQAAAg1JREFUSMet1TtolEEQB/Dfd3lhDJhoIeIDQRQCQhKCigpaiSgGrYIKaYJp7FOkFcRHK6SIWGiTyk5ETLQRrAVNo4WgEiNCHkIkp+bGZqNfjot38TIw7M7szvx3/zvMZv4t27ADWU5XpIRlvMPPtRI0VgE4j3t4g09oSP4izmIJuzCnDnmDh2nekrQ9Je2zAXIdr3P2ZnThVy3BjTXsKZXZL9MtGjYSIP+4l7ETjxNQsV6AZRRy9hRm8AObNgLgV9kNzmMLmtCK+XoBlsr4vp+oaURzteBCDQAnyuq8PVEzjTP1lugFBPZVWDuV1vb/b/I2fMG5Mn8TruBmWn+QfOuW23hSgedm3MJzPE1vdHxdmQsFpxGjo65GOJz0UITeCD0RDkTYG2H30JBrzc0WU2VVl4YGW/FhdFQpwrcIC0nnczoXYa6vz2x3t4UsE3hR6wXuHD0qIqrryIjo7xeDg6K1VRGXqiU/2dEhpqerJy+VVtsTEyLLvE3NcE3uv9+4Udvp80Dd3WL7dpHKdmwV5bn53c5OR8bH11dqWcbyMl1dHDvGq1d6i0WT+Jjf14KvExPrO30lHR4WqSGukoOYnZ+vH2BqSqRu257vRe+xNDlZ//f37BlYXOmyWY7LoaYmYwMDtLUR8ZfjSrxX8s3MMD5usVBwsVTySFmfhz3oSX3oT2yF+Vrj5/R/T68E/AZ5P2dgMqLZNQAAAABJRU5ErkJggg==","21.1" );
+        add_menu_page( 'Таблица масел', 'Calc-Oil', 'manage_options', 'calc-oils', 'get_oils_list',"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAQOFAAEDhQGlVDz+AAAAB3RJTUUH4gEDChQt3muYgQAAAg1JREFUSMet1TtolEEQB/Dfd3lhDJhoIeIDQRQCQhKCigpaiSgGrYIKaYJp7FOkFcRHK6SIWGiTyk5ETLQRrAVNo4WgEiNCHkIkp+bGZqNfjot38TIw7M7szvx3/zvMZv4t27ADWU5XpIRlvMPPtRI0VgE4j3t4g09oSP4izmIJuzCnDnmDh2nekrQ9Je2zAXIdr3P2ZnThVy3BjTXsKZXZL9MtGjYSIP+4l7ETjxNQsV6AZRRy9hRm8AObNgLgV9kNzmMLmtCK+XoBlsr4vp+oaURzteBCDQAnyuq8PVEzjTP1lugFBPZVWDuV1vb/b/I2fMG5Mn8TruBmWn+QfOuW23hSgedm3MJzPE1vdHxdmQsFpxGjo65GOJz0UITeCD0RDkTYG2H30JBrzc0WU2VVl4YGW/FhdFQpwrcIC0nnczoXYa6vz2x3t4UsE3hR6wXuHD0qIqrryIjo7xeDg6K1VRGXqiU/2dEhpqerJy+VVtsTEyLLvE3NcE3uv9+4Udvp80Dd3WL7dpHKdmwV5bn53c5OR8bH11dqWcbyMl1dHDvGq1d6i0WT+Jjf14KvExPrO30lHR4WqSGukoOYnZ+vH2BqSqRu257vRe+xNDlZ//f37BlYXOmyWY7LoaYmYwMDtLUR8ZfjSrxX8s3MMD5usVBwsVTySFmfhz3oSX3oT2yF+Vrj5/R/T68E/AZ5P2dgMqLZNQAAAABJRU5ErkJggg==","21.1" );
 });
 
 
-function get_oils() {
+function get_oils_list() { //oils (main) admin page 
     global $wpdb;
     $oils = $wpdb->get_results('SELECT * FROM '.$wpdb->prefix . 'co_oils');
     ?>
@@ -64,11 +77,11 @@ function get_oils() {
             </thead>
             <tbody> 
                 <?php foreach ($oils as $oil): ?>
-                <tr>
-                    <td><?php echo $oil->id; ?></td>
-                    <td><?php echo $oil->name; ?></td>
-                    <td><?php echo $oil->group; ?></td>
-                    <td><?php echo $oil->iodine; ?></td>
+                <tr id="row_<?php echo $oil->id; ?>">
+                    <td td-data="id"><?php echo $oil->id; ?></td>
+                    <td td-data="name"><?php echo $oil->name; ?></td>
+                    <td td-data="group"><?php echo $oil->o_group; ?></td>
+                    <td td-data="iodine"><?php echo $oil->iodine; ?></td>
                     <td><button class="btn btn-success oil-edit" btn-data="<?php echo $oil->id; ?>">Редактировать</td>
                 </tr>
                 <?php endforeach; ?>
@@ -86,36 +99,30 @@ function get_oils() {
                     </div>
                     <div class="modal-body">
                         <div class="form-row align-items-center">
-                            <div class="col-auto">
-                                <input name=" " value="" placeholder="Масло...">
-                            </div>                            
-                            <div class="col-auto">
-                                <label class="mr-sm-2" for="inlineFormCustomSelect">Группа</label>
-                                <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                    <option value="b0">b0</option>
-                                    <option value="pf1">pf1</option>
-                                    <option value="pf2">pf2</option>
-                                    <option value="b1">b1</option>
-                                    <option value="b2">b2</option>
-                                    <option value="b3">b3</option>
-                                    <option value="a1">a1</option>
-                                    <option value="a2">a2</option>
-                                    <option value="a3">a3</option>
-                                    <option value="z">z</option>
-                                </select>
+                            <div class="col-12">
+                                <input id="co_oil_name" value="" class="form-control" placeholder="Масло...">
                             </div>
                         </div>
                         <div class="form-row align-items-center">
-                            <table class="table table-striped ">
+                            <div class="col-auto">
+                                <label class="mr-sm-2" for="co_oil_group">Группа</label>
+                                <select class="form-control custom-select mr-sm-2" id="co_oil_group">                                    
+                                </select>
+                            </div>
+                            <div class="col-auto">
+                                <label class="mr-sm-2" for="co_oil_iodine">Йод</label>
+                                <input  class="form-control" type="text" id="co_oil_iodine">                                    
+                            </div>
+                        </div>
+                        <div class="form-row align-items-center">
+                            <table class="table table-striped" id="co_acids_table">
                                 <thead class="thead-dark">
                                     <tr>
                                         <th>#</th>
                                         <th>Наименование</th>
                                         <th>%</th>                                        
                                     </tr>
-                                </thead>
-                                <tbody>
-                                </tbody>
+                                </thead>                                
                             </table>
                         </div>
                     </div>
@@ -130,51 +137,38 @@ function get_oils() {
     </div>
     
 <?php
+} //end get_oils_list
 
-}
+add_action('wp_ajax_get_oils', function() use ($wpdb){ //get oils as json object
+    $oils = $wpdb->get_results("SELECT * FROM `".$wpdb->prefix."co_oils`");
+    foreach ($oils as &$oil){        
+        $oil->acids = $wpdb->get_results("SELECT id_acid as id, percent FROM `".$wpdb->prefix."co_oils_acids` WHERE id_oil ='".$oil->id."'");
+    }
 
-function calc_oil_adm_scripts(){
-    wp_register_style('bootstrap',plugins_url('/bootstrap/css/bootstrap.css',__FILE__));
-    wp_enqueue_style('bootstrap');
-    wp_enqueue_style( 'co-admin-style', plugins_url('/admin_style.css',__FILE__));
-    //js
+    $query = "SELECT * FROM `".$wpdb->prefix."co_acids`";
+    $acids = $wpdb->get_results($query);
     
-	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js');	
-    wp_register_script( 'popper',plugins_url('/bootstrap/js/popper.min.js',__FILE__),array('jquery'));
-    wp_register_script( 'bootstrap', plugins_url('/bootstrap/js/bootstrap.min.js',__FILE__),array('jquery'));
-    wp_register_script( 'co_admin-script', plugins_url('/js/co_admin.js',__FILE__),array('jquery'));
-  
-	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'popper' );
-	wp_enqueue_script( 'bootstrap' );
-	wp_enqueue_script( 'co_admin-script' );
-    wp_localize_script('co_admin-script', 'co_admin_ajax', 
-		array(			
-			'nonce' => wp_create_nonce('oc-adm-ajax-nonce')
-		)
-	);
+    $query = "SELECT COLUMN_TYPE 
+             FROM INFORMATION_SCHEMA.COLUMNS
+             WHERE TABLE_SCHEMA = '".$wpdb->dbname."'
+             AND TABLE_NAME = '".$wpdb->prefix."co_oils'
+             AND COLUMN_NAME = 'o_group'";
+    $groups = $wpdb->get_var($query);
+    $groups = explode(",",str_replace("'","",str_replace(")","",str_replace("enum(","",strtolower($groups)))));
 
-}
+    $query = "SELECT COLUMN_TYPE 
+             FROM INFORMATION_SCHEMA.COLUMNS
+             WHERE TABLE_SCHEMA = '".$wpdb->dbname."'
+             AND TABLE_NAME = '".$wpdb->prefix."co_acids'
+             AND COLUMN_NAME = 'type'";
+    $types = $wpdb->get_var($query);
+    $types = explode(",",str_replace("'","",str_replace(")","",str_replace("enum(","",strtolower($types)))));    
 
-add_action('wp_ajax_restruct_tabs', function(){
-   
-    echo json_encode("Something done. But result?");
-    wp_die();
-});
-add_action('wp_ajax_get_oil', function() use ($wpdb){
-    check_ajax_referer( 'oc-adm-ajax-nonce', 'nonce' );
-    $id = intval( $_POST['oil_id'] );
-    $res = $wpdb->get_results("SELECT id, `name` as acid, a.`type`, 0 as percent FROM `wp_co_acids` as `a` 
-                                WHERE a.id NOT IN (SELECT id_acid FROM `wp_co_oils_acids` as oa WHERE oa.id_oil =".$id.")
-                                UNION
-                                SELECT id_acid as id, `name`as acid, a.`type`, ROUND(`percent`,2) FROM `wp_co_acids` as `a` 
-                                LEFT JOIN `wp_co_oils_acids` as oa ON `a`.id = oa.id_acid
-                                WHERE oa.id_oil =".$id." ORDER BY `type`,id");
-    echo json_encode($res);
+    echo json_encode(array('nonce'=>wp_create_nonce('oc-adm-ajax-nonce'),'oils'=>$oils,'groups'=>$groups, 'types'=>$types, 'acids'=>$acids));
     wp_die();
 });
 
+/*--------------------------frontend-----------------------*/
 
  /*  
   * TODO: pagination in admin table
--- js/co_admin.js
+++ js/co_admin.js
@@ -1,20 +1,64 @@
-jQuery('document').ready(function($){
+var calc_oils={
+    'list':[],
+    'nonce':'',
+    'get_oil': function(id, callback){
+        if(calc_oils.list.length){
+            $.each(calc_oils.list,function(ind,oil){
+                if(oil.id == id){
+                    if(callback) callback(oil);
+                    else return oil;
+                }
+            });            
+        }
+    }    
+}
+jQuery('document').ready(function($){    
+    //ajax query for oils object
+    
+    $.post(ajaxurl,{'action':'get_oils'},function(result){
+        calc_oils.list = result.oils;
+        calc_oils.acids = result.acids;
+        calc_oils.nonce = result.nonce;
+        calc_oils.groups = result.groups;
+        calc_oils.types = result.types;
+        $.each(calc_oils.groups,function(ind,val){
+            $('#co_oil_group').append($('<option>').attr("value",val).text(val));
+        });
+        var grouped = {};
+        $.each(calc_oils.acids,function(ind,acid){
+            if(typeof(grouped[acid.type]) == "undefined") grouped[acid.type] = new Array();
+            grouped[acid.type].push(acid);
+            
+        });
+        $.each(calc_oils.types,function(ind,type){
+            var typeLocal = {'polyunsaturated':'полиненасыщенные','monounsaturated':'мононенасыщенные','saturated':'насыщенные'};
+            $('#co_acids_table').append($('<tbody>').attr('id',type).append($('<tr>').attr({'class':'clickable','data-toggle':'collapse','data-target':'#group-'+type,'aria-expanded':'false', 'aria-controls':'group-'+type}).append($('<td>').attr({'colspan':3}).text(typeLocal[type]))));
+            $('#co_acids_table').append($('<tbody>').attr({'id':'group-'+type,'class':'collapse'}));
+            if(typeof(grouped[type]) == "object"){
+                $.each(grouped[type],function(ind,gr_acid){
+                    $('#co_acids_table tbody#group-'+type).append($('<tr>')
+                        .append($('<td>').text(gr_acid.id))
+                        .append($('<td>').text(gr_acid.name))
+                        .append($('<td>').append($('<input>').attr('type','text'))))
+                });     
+            }
+        });        
+    },'json');
+    
+    //ajax get oil acids list, modal fill & show
     $('.oil-edit').each(function(){
         $(this).on('click',function(){
-            $.post(ajaxurl,{
-                'action':'get_oil',
-                'nonce':co_admin_ajax.nonce,
-                'oil_id':$(this).attr('btn-data')
-            },false,'json')
-            .done(function(result){
-                if(result.length){
-                    order = {'poly':[],'mono':[],'sat':[]};
-                    //result.each(function(acid){
-                        
-                    //})
-                    $('#co_oils_modal').modal('toggle');
-                }
-            });
-        })
+            calc_oils.get_oil($(this).attr('btn-data'), function(oil){
+                $('#co_oil_group').val(oil.o_group);
+                $("#co_oil_name").val(oil.name);
+                $("#co_oil_iodine").val(oil.iodine);
+                
+                $('#co_oils_modal').modal('toggle');
+            });            
+        });
     });
+    //modal clear & save 
+    $('#co_oils_modal').on('hidden.bs.modal', function (e) {
+        
+    })
 });
